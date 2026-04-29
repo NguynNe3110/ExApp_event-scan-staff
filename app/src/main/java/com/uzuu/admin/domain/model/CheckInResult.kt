035d077dabce0d19a2979e0af92a3550812958cd
NguyenUzuu <nguyen12112005@gmail.com>
2026-04-29 14:40:08 +0700

    navigate to forgot & login

--- app/src/main/java/com/uzuu/admin/domain/model/CheckInResult.kt
+++ app/src/main/java/com/uzuu/admin/domain/model/CheckInResult.kt
@@ -1,17 +1,5 @@
 package com.uzuu.admin.domain.model
 
-data class Login(val username: String, val password: String)
-
-data class Register(
-    val username: String,
-    val password: String,
-    val email: String,
-    val fullName: String,
-    val organizeId: Long? = null
-)
-
-data class ForgotPassword(val email: String)
-
 data class CheckInResult(
     val id: Long,
     val eventName: String,
